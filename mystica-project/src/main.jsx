4a0d6c47a95df2c64753a52f80ee9777261e28e4
sonnzxx3-dot <sonnzxx3@gmail.com>
2026-06-01 05:04:10 +0300

    Delete mystica-project directory

--- mystica-project/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MysticApp from "./MysticApp.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MysticApp />
-  </React.StrictMode>
-);
